562dd8ed020f2bcb1263d950fce3670cb8a0c4a4
Victor Bos <victor.bos@gmail.com>
2020-01-06 21:54:40 +0200

    Parsing Empty, Deadlock, and Atom.

--- stack-project/app/Main.hs
+++ stack-project/app/Main.hs
@@ -7,9 +7,10 @@ import qualified Lib as L
 import System.Console.Haskeline
 
 main :: IO ()
-main = runInputT defaultSettings $ do
-  line <- getInputLine ""
-  case line of
-    Just s -> outputStrLn $ show (L.parseProcess (pack s))
-    _      -> outputStrLn $ "?!#?"
-    
+main = do
+  runInputT defaultSettings $ do
+    line <- getInputLine ""
+    case line of
+      Just s -> outputStrLn $ show (L.parseProcess (pack s))
+      _      -> outputStrLn $ "?!#?"
+  main    
--- stack-project/src/Lib.hs
+++ stack-project/src/Lib.hs
@@ -2,11 +2,11 @@
 
 module Lib
     ( Process (..),
-      processParser,
       parseProcess
     ) where
 
 import Control.Applicative
+import Data.Char
 import Data.Text
 import Data.Attoparsec.Text
 
@@ -20,7 +20,21 @@ data Process = Atom Text
              deriving (Eq, Show)
 
 processParser :: Parser Process
-processParser = Empty <$ (string "Empty") <* endOfInput
+processParser = simpleProcess <* endOfInput
+
+simpleProcess :: Parser Process
+simpleProcess = emptyParser <|> deadlockParser <|> atomParser
+
+emptyParser :: Parser Process
+emptyParser = Empty <$ (string "!")
+
+deadlockParser :: Parser Process
+deadlockParser = Deadlock <$ (string "?")
+
+atomParser :: Parser Process
+atomParser = Atom <$> do
+  s <- many1 (letter <|> digit)
+  return (pack s)
 
 parseProcess :: Text -> Process
 parseProcess t = toProcess (parseOnly processParser t)
